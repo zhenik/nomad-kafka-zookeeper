e40abd4e5b3c8d5ceef2241a8e4e31533c310ab4
zhenik <nikita.zhevnitskiy@gmail.com>
2020-01-07 14:25:55 +0100

    update custom docker image

--- docker/zoo-custom/docker-entrypoint.sh
+++ docker/zoo-custom/docker-entrypoint.sh
@@ -6,28 +6,41 @@ set -e
 # sleep for 60s to allow nomad services to be registered
 sleep 20
 
-## create the zookeeper dynamic cfg from consul template
-#if [[ -z "$CONSUL_HTTP_ADDR" ]]; then
-#  consul-template -once -consul-addr=${CONSUL_HTTP_ADDR} -template /consul-templates/zookeeper-services.ctpl:$ZOO_CONF_DIR/zoo.cfg.dynamic
-#else
-#  consul-template -once -template /consul-templates/zookeeper-services.ctpl:$ZOO_CONF_DIR/zoo.cfg.dynamic
-#fi
-
-# decode the base64 truststore and keystore files
-#base64 -d $ZOO_CONF_DIR/ssl/keystore_base64.p12 > /conf/ssl/keystore.p12
-#base64 -d $ZOO_CONF_DIR/ssl/truststore_base64.jks > /conf/ssl/trustore.jks
-
-# set myid based on ip match in generated $ZOO_CONF_DIR/zoo.cfg.dynamic
-# myid=
-# grep "10.102.45.113" job_experiments/zoo.cfg.dynamic | egrep -o '[0-9]=' | cut -c 1
-#myhost=`cat $ZOO_CONF_DIR/my_host`
-#grep "$myhost" $ZOO_CONF_DIR/zoo.cfg.dynamic | egrep -o '[0-9]=' | cut -c 1 > $ZOO_DATA_DIR/myid
-#chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-
 # Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
     exec gosu zookeeper "$0" "$@"
+fi
+
+# Generate the config only if it doesn't exist
+if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
+    CONFIG="$ZOO_CONF_DIR/zoo.cfg"
+
+    echo "dataDir=$ZOO_DATA_DIR" >> "$CONFIG"
+    echo "dataLogDir=$ZOO_DATA_LOG_DIR" >> "$CONFIG"
+
+    echo "tickTime=$ZOO_TICK_TIME" >> "$CONFIG"
+    echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
+    echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
+
+    echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT" >> "$CONFIG"
+    echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL" >> "$CONFIG"
+    echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
+    echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED" >> "$CONFIG"
+    echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED" >> "$CONFIG"
+
+    if [[ -z $ZOO_SERVERS ]]; then
+      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
+    fi
+
+    for server in $ZOO_SERVERS; do
+        echo "$server" >> "$CONFIG"
+    done
+
+    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
+        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
+    fi
+
 fi
 
 # source in SERVER_JVMFLAGS and CLIENT_JVMFLAGS
